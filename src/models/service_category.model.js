f975164e9b896020c89b6cbf0a69aaecc78dd1e0
Malik Majeed <malikmajeedzia@gmail.com>
2025-11-28 15:10:30 +0500

    service Category model for storing cats.

---
+++ src/models/service_category.model.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+const db = require("../database");
+
+const ServiceCategory = db.define("SERVICE_CATEGORY", {
+    category_ID: {
+        type: DataTypes.STRING(6),
+        primaryKey: true,
+        allowNull: false,
+        unique: true
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = ServiceCategory;
