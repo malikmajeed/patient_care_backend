dbf290e9096aeec7447ae4eedf80485a513b6b23
Malik Majeed <malikmajeedzia@gmail.com>
2025-11-29 01:22:00 +0500

    create user function added

--- src/services/patient.service.js
+++ src/services/patient.service.js
@@ -1,12 +1,38 @@
 const Patient = require("../models/patient.model");
+const { createPatientSchema } = require("../schema/patient.schema");
 
 
-const signup = async (req, res) => {
+
+const create = async (userData) => {
     try {
+        const { error, value } = createPatientSchema.validate(userData);
 
-        const { first_name, last_name, email, password, phone_number, address } = req.body;
+        if (error) {
+            throw new Error(error.details[0].message);
+        }
 
+        //temporary patient ID generation function
+        const generatePatientId = () => {
+            const randomNum = Math.floor(Math.random() * 10000)   // 0 to 9999
+                .toString()
+                .padStart(4, '0'); // ensures 4 digits
+
+            return "PT" + randomNum;
+        };
+
+        value.patient_ID = generatePatientId();
+        const patient = await Patient.create(value);
+        return patient;
     } catch (error) {
-
+        throw new Error(error.message);
     }
 }
+
+
+
+
+const PatientService = {
+    create
+};
+
+module.exports = PatientService;
