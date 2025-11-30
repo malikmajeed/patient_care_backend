e7dfb23efea402cc07e4dd072c8b27ffeb4337a9
Malik Majeed <malikmajeedzia@gmail.com>
2025-11-30 22:23:23 +0500

    updated admin route with patch

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -5,7 +5,7 @@ const adminController = require("../controllers/admin.controller");
 
 router.post("/signup", adminController.signup);
 router.post("/login", adminController.login);
-
+router.patch("/update/:id", adminController.update);
 
 
 
