e14c840e2ce845e00b6c34ce22dcf54b4b7d4dc5
Malik Majeed <malikmajeedzia@gmail.com>
2025-11-30 21:04:36 +0500

    login route added

---
+++ src/routes/admin.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+const adminController = require("../controllers/admin.controller");
+
+router.post("/signup", adminController.signup);
+router.post("/login", adminController.login);
+
+
+
+
+module.exports = router;
